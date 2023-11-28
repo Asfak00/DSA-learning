bc72c0d4c75a0dedb2035911010743afc2103269
Asfak Ahmed <asfakahmed680@gmail.com>
2023-11-28 18:41:58 +0600

    fibonacci

---
+++ Fibonacci/index.js
@@ -0,0 +1,12 @@
+function fibonacci(n) {
+  let fib = [0, 1];
+  for (let i = 2; i < n; i++) {
+    fib[i] = fib[i - 1] + fib[i - 2];
+  }
+
+  return fib;
+}
+
+console.log(fibonacci(2));
+console.log(fibonacci(3));
+console.log(fibonacci(7));
