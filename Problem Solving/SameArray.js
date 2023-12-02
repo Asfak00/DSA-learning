34e8b8e68f5603a1743ffe2d8fb6b9f2d07fccee
Asfak Ahmed <asfakahmed680@gmail.com>
2023-12-02 18:11:13 +0600

    same array

---
+++ Problem Solving/SameArray.js
@@ -0,0 +1,26 @@
+// check the array1 and array2 are same or not
+function sameArray(array1, array2) {
+  // check the length of array1 and array2
+  if (array1.length !== array2.length) {
+    return false;
+  }
+
+  // loop through the array1
+  for (let i = 0; i < array1.length; i++) {
+    // find the current index of array2 element
+    let currentIndex = array2.indexOf(array1[i] ** 2);
+
+    // if the current index is not found then return false
+    if (currentIndex === -1) {
+      return false;
+    } else {
+      // if there has a value in current index variable then remove the value from array2
+      array2.splice(currentIndex, 1);
+    }
+  }
+
+  // if all the value of array1 is found in array2 then return true
+  return true;
+}
+
+console.log(sameArray([2, 3], [4, 9, 36])); // false
