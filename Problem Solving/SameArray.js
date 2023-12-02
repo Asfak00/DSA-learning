3efdc31f49855b08b0d28cf987c87232728062fa
Asfak Ahmed <asfakahmed680@gmail.com>
2023-12-02 20:53:44 +0600

    readme file updated

--- Problem Solving/SameArray.js
+++ Problem Solving/SameArray.js
@@ -25,31 +25,26 @@
 
 // console.log(sameArray([2, 3], [4, 9, 36])); // false
 
-// // using find method
-// function sameArray(array1, array2) {
-//   // check the length of array1 and array2
-//   if (array1.length !== array2.length) {
-//     return false;
-//   }
+// using find method
+function sameArray(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
 
-//   // loop through the array1
-//   for (let i = 0; i < array1.length; i++) {
-//     // find the current index of array2 element
-//     let matchedElement = array2.find((element) => element === array1[i] ** 2);
+  for (let i = 0; i < array1.length; i++) {
+    let matchedElement = array2.find((element) => element === array1[i] ** 2);
 
-//     if (!matchedElement) {
-//       return false;
-//     } else {
-//       // if there has a value in current index variable then remove the value from array2
-//       array2.splice(array2.indexOf(matchedElement), 1);
-//     }
-//   }
+    if (!matchedElement) {
+      return false;
+    } else {
+      array2.splice(array2.indexOf(matchedElement), 1);
+    }
+  }
 
-//   // if all the value of array1 is found in array2 then return true
-//   return true;
-// }
+  return true;
+}
 
-// console.log(sameArray([2, 3, 6], [4, 9, 37, 4])); // false
+console.log(sameArray([2, 3, 6], [4, 9, 37, 4])); // false
 
 // |
 // |
