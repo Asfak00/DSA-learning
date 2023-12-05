54fec4e436f463ec83faf69da81e19af3e36d3e1
Asfak Ahmed <asfakahmed680@gmail.com>
2023-12-05 19:32:09 +0600

    max water container problem solving

--- Problem Solving/MaxWater.js
+++ Problem Solving/MaxWater.js
@@ -1,18 +1,18 @@
-function mostWater(arr) {
-  let maxWater = 0;
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = i + 1; j < arr.length; j++) {
-      let height = Math.min(arr[i], arr[j]);
+// function mostWater(arr) {
+//   let maxWater = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = i + 1; j < arr.length; j++) {
+//       let height = Math.min(arr[i], arr[j]);
 
-      width = j - i;
+//       width = j - i;
 
-      let currentWater = height * width;
+//       let currentWater = height * width;
 
-      maxWater = Math.max(currentWater, maxWater);
-    }
-  }
+//       maxWater = Math.max(currentWater, maxWater);
+//     }
+//   }
 
-  return maxWater;
-}
+//   return maxWater;
+// }
 
-console.log(mostWater([1, 8, 6, 2, 5, 4, 8, 3, 7]));
+// console.log(mostWater([1, 8, 6, 2, 5, 4, 8, 3, 7]));
